98244a2833aeec20aa9d9116b5c0beea40f97d6a
ZerolCamb <zerolcamb@outlook.com>
2021-08-16 17:04:58 -0500

    Tarea 1B

---
+++ ANALISIS Y DISEÑO DE ALGORITMOS/TAREA 1B/Tarea_1B.c
@@ -0,0 +1,28 @@
+/*Creado por Mejia Bazan Cesar Arturo 2182005565*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int producto(int a, int b);
+int contador = 0;
+
+int main(){
+    int a, b;
+    scanf("%d %d", &a, &b);
+    producto(a,b);
+    printf("%d", contador);
+    return 0;
+}
+
+int producto(int a,  int b){
+    if(a <= 0) return 0;
+    else {
+        if( a % 2) {
+            contador += 3;
+            return 2*producto(a/2, b) + b;
+        } else {
+            contador += 2;
+            return 2*producto(a/2,b);
+        }
+    }
+}
