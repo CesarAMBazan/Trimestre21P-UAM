a7af2004e20bbcdad082ddbc426d3ded7d574107
ZerolCamb <zerolcamb@outlook.com>
2021-09-04 03:54:45 -0500

    Programas del primer examen

---
+++ ANALISIS Y DISEÑO DE ALGORITMOS/Examen1/examen1.c
@@ -0,0 +1,16 @@
+/*Creado por Mejia Bazan Cesar Arturo 2182005565*/
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int b, c, n, k, t, w, x, y, z;
+    scanf("%d %d %d", &b, &c, &n);
+    k= log10(n)/log10(b);
+    t= n;
+    w= k;
+    x= b;
+    y= k-1;
+    z= c;
+    printf("%d %d %d %d %d", t, w, x, y, z);
+    return 0;
+}
