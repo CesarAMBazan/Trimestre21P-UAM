4488ba67ac74cea3809c7edd0f788e95fa450855
ZerolCamb <zerolcamb@outlook.com>
2021-09-28 04:44:11 -0500

    Solucion problemas de secuencias

--- Analisis-Algoritmos/Busqueda con Retroceso/SecuenciasCrecientes/secuencias.cpp
+++ Analisis-Algoritmos/Busqueda con Retroceso/SecuenciasCrecientes/secuencias.cpp
@@ -4,45 +4,26 @@
  */
 
 #include <stdio.h>
-#include <algorithm>
 
-/* ESP - ENG
- * contador = counter
- * secuencias = sequences
- * choices = choices
- */
 int contador, n, k;
 int secuencias[15];
-int choices[25];
 void solve(int i)
 {
-    if (n >= k)
+    if (i == n)
     {
-        if (n == k)
-            contador = 1;
-        return;
+        //! DEBUG: Imprimir secuencias
+        // for (int i = 0; i < n; i++)
+        //     printf("%d ", secuencias[i]);
+        // printf("\n");
+        contador++;
     }
     else
     {
-        if (i == n)
+        for (int c = i + 1; c <= k; ++c)
         {
-            //! DEBUG: Print sequences
-            // for (int i = 0; i < n; i++)
-            //     printf("%d ", secuencias[i]);
-            // printf("\n");
-            contador++;
-        }
-        else
-        {
-            for (int c = 0; c < k; ++c)
-            {
-                // fill 'sequences' position with all of choices numbers
-                std::swap(secuencias[i], choices[c]);
-                // if the number in the current position is greater than the last one
-                if (i == 0 || (secuencias[i] - secuencias[i - 1]) >= 1)
-                    solve(i + 1);                     // go to the next position
-                std::swap(secuencias[i], choices[c]); // reset
-            }
+            secuencias[i] = c;
+            if (i == 0 || (secuencias[i] - secuencias[i - 1]) >= 1)
+                solve(i + 1);
         }
     }
 }
@@ -50,10 +31,6 @@ void solve(int i)
 int main()
 {
     scanf("%d %d", &n, &k);
-    for (int i = 0; i < k; i++)
-    {
-        choices[i] = i + 1;
-    }
     solve(0);
     printf("%d", contador);
     return 0;
