cd9f8a39de85d7a0db8b46af7e79c2c3ae8d8d95
ZerolCamb <zerolcamb@outlook.com>
2021-10-09 20:44:18 -0500

    aver

--- Analisis-Algoritmos/Programacion-dinamica/Coeficiente-Binomial/Coeficiente_dinamica.cpp
+++ Analisis-Algoritmos/Programacion-dinamica/Coeficiente-Binomial/Coeficiente_dinamica.cpp
@@ -13,24 +13,37 @@ struct resultado
     int llamadas;
 };
 
-resultado f(int n, int k)
-{
+// resultado f(int n, int k)
+// {
 
-    if (k == 0 || k == n)
-        return {1, 1};
-    else
-    {
-        resultado r1 = f(n - 1, k - 1);
-        resultado r2 = f(n - 1, k);
-        return {r1.valor + r2.valor, r1.llamadas + r2.llamadas + 1};
-    }
-}
+//     if (k == 0 || k == n)
+//         return {1, 1};
+//     else
+//     {
+//         resultado r1 = f(n - 1, k - 1);
+//         resultado r2 = f(n - 1, k);
+//         return {r1.valor + r2.valor, r1.llamadas + r2.llamadas + 1};
+//     }
+// }
 
 int main()
 {
-    int n, k;
-    scanf("%d %d", &n, &k);
-    resultado r = f(n, k);
-    printf("%d %d", r.valor, r.llamadas);
-    return 0;
+    int n0, k0;
+    scanf("%d %d", &n0, &k0);
+    resultado memoria[n0 + 1][k0 + 1];
+    for (int n = 0; n <= n0; n++)
+    {
+        for (int k = 0; k <= n; k++)
+        {
+            if (k == 0 || k == n)
+                memoria[n][k] = {1, 1};
+            else
+            {
+                resultado r1 = memoria[n - 1][k - 1];
+                resultado r2 = memoria[n - 1][k];
+                memoria[n][k] = {r1.valor + r2.valor, r1.llamadas + r2.llamadas + 1};
+            }
+        }
+    }
+    printf("%d %d", memoria[n0][k0].valor, memoria[n0][k0].llamadas);
 }
